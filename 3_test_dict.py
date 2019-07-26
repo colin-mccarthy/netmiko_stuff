488055a7c6c32935406761534f388abcfdbf43f0
Colin McCarthy <colin.mccarthy80501@gmail.com>
2019-07-26 19:22:55 -0400

    one

---
+++ 3_test_dict.py
@@ -0,0 +1,14 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+device1 = {
+    "host": '192.168.161.15', 
+    "username": 'pi', 
+    "password": getpass(), 
+    "device_type": 'cisco_ios',
+ #   "session_log": 'my_session.txt',
+}
+
+
+net_connect = ConnectHandler(**device1)
+print(net_connect.find_prompt())
