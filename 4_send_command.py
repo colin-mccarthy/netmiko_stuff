7486bc42a2bffec67157086ff3c6469b89089d4a
Colin McCarthy <colin.mccarthy80501@gmail.com>
2019-07-26 20:05:48 -0400

    one

---
+++ 4_send_command.py
@@ -0,0 +1,17 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+device1 = {
+    "host": '192.168.161.15', 
+    "username": 'pi', 
+    "password": getpass(), 
+    "device_type": 'cisco_ios',
+ #   "session_log": 'my_session.txt',
+}
+
+
+net_connect = ConnectHandler(**device1)
+print(net_connect.find_prompt())
+
+output = net_connect.send_command("show ip int brief")
+print(output)
